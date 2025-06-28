11a7ffa7b4a9fa2206b7c0c6fc6c25e2d10585f3
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-06-28 19:44:46 -0400

    feat: implement CLI prompt input with argument validation

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
     args = sys.argv[1:]
 
@@ -17,17 +21,18 @@ def main():
         sys.exit(1)
     user_prompt = " ".join(args)
 
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt,
+        contents=messages,
     )
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-    print("Prompt tokens;", prompt_tokens)
-    print("Response tokens:", response_tokens)
     print("Response:")
     print(response.text)
 
